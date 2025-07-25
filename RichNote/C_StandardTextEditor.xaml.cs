0900fc9da9966bfc5c1c9ac17d109b8f73e7a64d
VeeTHis <vicjthom1@gmail.com>
2025-07-25 03:59:09 -0400

    Implement TXT editor controls

--- RichNote/C_StandardTextEditor.xaml.cs
+++ RichNote/C_StandardTextEditor.xaml.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,20 +5,56 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
 
 namespace RichNote;
 
 public sealed partial class StandardTextEditor : UserControl, IEditorControl
 {
+    // Initialization
+    private TextBox editor;
+    private double zoomFactor = 1.0;
+
     public StandardTextEditor()
     {
         InitializeComponent();
+        editor = MyEditorTextBox;
     }
 
     public CommandBar EditorCommandBar => MyEditorCommandBar;
     public RichEditBox? EditorRichEditBox => null;
     public TextBox? EditorTextBox => MyEditorTextBox;
+
+    // Event handlers
+    private void ZoomIn_Click(object sender, RoutedEventArgs e)
+    {
+        zoomFactor += 0.5;
+        editor.RenderTransform = new ScaleTransform { ScaleX = zoomFactor, ScaleY = zoomFactor };
+    }
+
+    private void ZoomOut_Click(object sender, RoutedEventArgs e)
+    {
+        if (zoomFactor > 1)
+        {
+            zoomFactor -= 0.5;
+            editor.RenderTransform = new ScaleTransform { ScaleX = zoomFactor, ScaleY = zoomFactor };
+        }
+    }
+
+    private void Undo_Click(object sender, RoutedEventArgs e)
+    {
+        editor.Undo();
+    }
+
+    private void Redo_Click(object sender, RoutedEventArgs e)
+    {
+        editor.Redo();
+    }
 }
